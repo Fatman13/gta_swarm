576b9253d17b5186768d5eab446e9958a6d51311
fatman13 <tctctcly@gmail.com>
2017-02-24 17:50:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# GTA SWARM
+
+A simple python scrript to run n times of AddBookingRequest and CancelBookingRequest by response from SearchHotelPriceRequest.
+
+# Install
+
+1. Install python 3.6. (Remember to check add to PATH when installing)
+2. Download and extract the content. 
+3. Open `cmd`, `cd` to the directory with extracted script. (Try `python --version`, if something went wrong try add python to system PATH)
+4. run `pip install requests`
+
+# Run
+
+1. Open `cmd`, within the directory of the script, run `python swarm.py` 
+2. If you want output to be redirected to a file, run `python swarm.py > output`
+
+Note: script not well tested, might break at any point.
+
+# Config
+
+1. `url`(request url) and `counter`(number of booking request) could be configed at the top of swarm.py script.
+2. user credentials could be configed in SearchHotelPriceRequest.xml. The script will take credentials from SearchHotelPriceRequest.xml and override whatever is in AddBookingRequest.xml, and CancelBookingRequest.xml.
+3. The script will try to keep some of the critical xml element consistent through the booking.
+4. It is not recommended to remove any XML element or XML attributes in AddBookingRequest.xml, CancelBookingRequest.xml or SearchHotelPriceRequest.xml. It is also recommended that to keep Room element as it is. 
+
+# Question?
+
+Please send an email to <mailto:yu.leng@gta-travel.com>.
---
+++ swarm.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+import pprint
+import os
+import uuid
+from xml.etree import ElementTree as ET
+import requests
+
+# config variables 
+url = 'https://interface.demo.gta-travel.com/rbshkapi/RequestListenerServlet'
+counter = 10
+
+Ids = []
+trees = []
+num_add_suc = 0
+num_cancel_suc = 0
+
+pp = pprint.PrettyPrinter(indent=4)
+
+pp.pprint(str('/// /// /// Start Swarming /// /// ///'))
+pp.pprint('Current WD path: ' + os.getcwd())
+
+
+# datafile = None
+# with open(os.path.join(os.getcwd(), 'AddBookingRequest.xml'), 'r') as f:
+# 	datafile = f.read()
+
+# pp.pprint(datafile)
+
+add_tree = ET.parse(os.path.join(os.getcwd(), 'AddBookingRequest.xml'))
+cancel_tree = ET.parse(os.path.join(os.getcwd(), 'CancelBookingRequest.xml'))
+search_tree = ET.parse(os.path.join(os.getcwd(), 'SearchHotelPriceRequest.xml'))
+
+checkin_date = search_tree.find('.//CheckInDate').text
+search_tree.find('.//NumberOfReturnedItems').text = str(counter)
+credentials = dict([('client', search_tree.find('.//RequestorID').get('Client')), 
+	('email_address', search_tree.find('.//RequestorID').get('EMailAddress')), 
+	('password', search_tree.find('.//RequestorID').get('Password'))])
+
+preferences = dict([('country', search_tree.find('.//RequestorPreferences').get('Country')), 
+	('currency', search_tree.find('.//RequestorPreferences').get('Currency')), 
+	('language', search_tree.find('.//RequestorPreferences').get('Language'))])
+
+trees.append(add_tree)
+trees.append(cancel_tree)
+
+for tree in trees:
+	tree.find('.//RequestorID').set('Client', credentials['client'])
+	tree.find('.//RequestorID').set('EMailAddress', credentials['email_address'])
+	tree.find('.//RequestorID').set('Password', credentials['password'])
+	tree.find('.//RequestorPreferences').set('Country', preferences['country'])
+	tree.find('.//RequestorPreferences').set('Currency', preferences['currency'])
+	tree.find('.//RequestorPreferences').set('Language', preferences['language'])
+
+pp.pprint('Passenger checkin_date: ' + checkin_date)
+
+pp.pprint('////// Search Hotel Price Request ////// ')
+
+r = requests.post(url, data=ET.tostring(search_tree.getroot(), encoding='UTF-8', method='xml'))
+
+search_r_tree = ET.fromstring(r.text)
+pp.pprint('////// Getting Ids from Search Hotel Price Response ////// ')
+
+for hotel in search_r_tree.find('.//HotelDetails'):
+	city_code = hotel.find('.//City').get('Code')
+	item_code = hotel.find('.//Item').get('Code')
+	for room_category in hotel.find('.//RoomCategories'):
+		# Ids.append(room_category.get('Id'))
+		Ids.append(dict([('Id', room_category.get('Id')), ('cityCode', city_code), ('itemCode', item_code)]))
+
+pp.pprint('list of Ids (' + str(len(Ids)) + '): ' + ''.join(str(d) for d in Ids))
+
+if len(Ids) < counter:
+	pp.pprint("Error: length of Ids is less than counter...")
+
+for i in range(counter):
+	book_ref = str(uuid.uuid4())[:20];
+
+	pp.pprint(str(i) + 'Random Booking Reference: ' + str(book_ref))
+	add_tree.find('.//BookingReference').text = book_ref
+	# to be changed
+	add_tree.find('.//BookingDepartureDate').text = checkin_date
+	add_tree.find('.//CheckInDate').text = checkin_date
+	# set booking details dict
+	item_info = Ids.pop()
+	add_tree.find('.//HotelRoom').set('Id', item_info['Id'])
+	add_tree.find('.//ItemCity').set('Code', item_info['cityCode'])
+	add_tree.find('.//Item').set('Code', item_info['itemCode'])
+
+	cancel_tree.find('.//BookingReference').text = book_ref
+	# add_tree.write('test1.xml')
+	# cancel_tree.write('test2.xml')
+	# add_tree.
+	# ET.tostring(element, encoding, method)
+	# pp.pprint(ET.tostring(add_tree.getroot(), encoding='utf8', method='xml'))
+	# root = add_tree.getroot()
+	# xmlstr = ET.tostring(root)
+	# pprint(xmlstr)
+
+	r = requests.post(url, data=ET.tostring(add_tree.getroot(), encoding='UTF-8', method='xml'))
+	# r.text
+	pp.pprint('////// Add Booking Response ////// ' + str(i))
+	if r.status_code == 200:
+		num_add_suc += 1
+	pp.pprint('Add Booking Response status code: ' + str(r.status_code))
+	pp.pprint(r.headers)
+	pp.pprint('Add Booking Response body: ' + r.text)
+
+
+	r = requests.post(url, data=ET.tostring(cancel_tree.getroot(), encoding='UTF-8', method='xml'))
+	pp.pprint('////// Cancel Booking Response ////// ' + str(i))
+	if r.status_code == 200:
+		num_cancel_suc += 1
+	pp.pprint('Cancel Booking Response status code: ' + str(r.status_code))
+	pp.pprint(r.headers)
+	pp.pprint('Cancel Booking Response body: ' + r.text)
+
+pp.pprint('/// /// /// Test Result /// /// ///')
+pp.pprint(str(num_add_suc) + ' out of ' + str(counter) + ' AddBookingrequest got HTTP 200')
+pp.pprint(str(num_cancel_suc) + ' out of ' + str(counter) + ' CancelBookingrequest got HTTP 200')
