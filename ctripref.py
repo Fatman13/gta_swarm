890847e076acc0253700c897e58a4b9dcee9795f
Yu Leng <tctctcly@gmail.com>
2017-09-11 10:29:34 +0800

    more checks in ctripref

--- ctripref.py
+++ ctripref.py
@@ -20,49 +20,55 @@ import sys
 import datetime
 import re
 
+def is_bad_date(filename_regex, newest):
+	today_date = datetime.datetime.now().strftime('%y%m%d')
+	try:
+		newest_date = re.search(filename_regex, newest).group(1)
+	except AttributeError:
+		newest_date = ''
+	if newest_date != today_date:
+		print('Error: newest date != today date.. mannual intervention needed..')
+		return False
+	print('newest date: ' + newest_date)
+	return True
+
+def hua_style_sleep():
+	for i in range(3):
+		print('sleeping..' + str(i))
+		time.sleep(1)
+
 @click.command()
 @click.option('--days', default=0, type=int)
 @click.option('--duration', default=0, type=int)
 # @click.option('--days', default=1, type=int)
 def ctripref(days, duration):
 
-	# python booking_id.py --days 0 --duration 0 --client ctrip --d_type departure
-	# subprocess.call(['python', 'booking_id.py', '--days', str(days), '--duration', str(duration), '--client', 'ctrip', '--d_type', 'departure'])
 	subprocess.call(['python', 'booking_id_ctrip.py', '--days', str(days), '--duration', str(duration), '--d_type', 'departure'])
 
-	for i in range(3):
-		print('sleeping..')
-		time.sleep(1)
-
 	newest = max(glob.iglob('output_Search_booking_id_*.csv'), key=os.path.getctime)
+	if is_bad_date('output_Search_booking_id_(\d+)', newest):
+		print('Error: bad date.. ')
+		return
 	subprocess.call(['python', 'search_item_hr.py', '--filename', newest])
 
-	for i in range(3):
-		print('sleeping..')
-		time.sleep(1)
+	hua_style_sleep()
 
 	newest = max(glob.iglob('output_Search_item_hr_*.csv'), key=os.path.getctime)
+	if is_bad_date('output_Search_item_hr_(\d+)', newest):
+		print('Error: bad date.. ')
+		return
 	subprocess.call(['python', 'hc.py', '--filename', newest])
 
-	for i in range(3):
-		print('sleeping..')
-		time.sleep(1)
+	hua_style_sleep()
 
 	# newest = max(glob.iglob('output_Search_item_hr_*.csv'), key=os.path.getctime)
 	# subprocess.call(['python', 'sendmail.py', '--filename', 'output_hotel_ref_*.csv', '--title', 'Ctrip_hotel_ref'])
 
 	newest = max(glob.iglob('output_hotel_ref_*.csv'), key=os.path.getctime)
 
-	today_date = datetime.datetime.now().strftime('%y%m%d')
-	try:
-		newest_date = re.search('output_hotel_ref_(\d+)', newest).group(1)
-	except AttributeError:
-		newest_date = ''
-	if newest_date != today_date:
-		print('Error: newest date != today date.. mannual intervention needed..')
+	if is_bad_date('output_hotel_ref_(\d+)', newest):
+		print('Error: bad date.. ')
 		return
-
-	print('newest date: ' + newest_date)
 
 	# while True:
 	# 	sys.stdout.write("Would you like to proceed to call Ctrip's update hotel res no API? " + newest + " [Y/N]")
@@ -73,7 +79,6 @@ def ctripref(days, duration):
 	# 		return
 
 	subprocess.call(['python', 'ctrip_update_res_no.py', '--filename', newest])
-
 
 if __name__ == '__main__':
 	ctripref()
