bf4e1674a5f79dc4a393b713ed057d76245ba846
Yu Leng <tctctcly@gmail.com>
2017-09-19 11:47:16 +0800

    asp now should properly search price

--- asp.py
+++ asp.py
@@ -13,6 +13,9 @@ import os
 import random
 import json
 # import logging
+from requests.exceptions import ConnectionError
+from requests.exceptions import ChunkedEncodingError
+from requests.exceptions import ReadTimeout
 
 def validate_d(date_text):
 	try:
@@ -23,6 +26,14 @@ def validate_d(date_text):
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
+
+MAX_RETRIES = 3
+
+def has_item_price(r):
+	r_tree = ET.fromstring(r.text)
+	if r_tree.find('.//ItemPrice') == None:
+		return False
+	return True
 
 @click.command()
 @click.option('--file_name', default='gta_hotel_keys')
@@ -86,28 +97,48 @@ def asp(file_name, from_d, to_d, client):
 
 			# pprint.pprint(ET.tostring(search_tree.getroot(), encoding='UTF-8', method='xml'))
 
-			try:
-				r = requests.post(url, data=ET.tostring(search_tree.getroot(), encoding='UTF-8', method='xml'), timeout=360)
-			except OSError:
-				pp.pprint('Error: ignoring OSError...')
+			r = None
+			# r_tree = None
+			for i in range(MAX_RETRIES):
+				try:
+					r = requests.post(url, data=ET.tostring(search_tree.getroot(), encoding='UTF-8', method='xml'), timeout=30)
+					# retry if no price found?
+					if not has_item_price(r):
+						continue
+
+				except OSError:
+					pp.pprint('Error: ignoring OSError...')
+					continue
+				except ConnectionError:
+					pp.pprint('Error: ignoring ConnectionError...')
+					continue
+				except ChunkedEncodingError:
+					pp.pprint('Error: ignoring ChunkedEncodingError...')
+					continue
+				except ReadTimeout:
+					pp.pprint('Error: ignoring ReadTimeout...')
+					continue
+				else:
+					break
+
+			if r == None:
+				print('Warning: Reached MAX RETRIES.. still no good.. ')
 				continue
-
 			# pprint.pprint(r.text)
-
-			r_tree = ET.fromstring(r.text)
 
 			# pprint.pprint(r_tree.getroot())
 		
-			if (r_tree.find('.//RoomPrice') == None):
+			if not has_item_price(r):
 				# hotel_code['missing_price'].append('Pax ' + str(i + 1) + ': ' + single_date.strftime("%Y-%m-%d"))
-				pp.pprint('Alert: Price not returned... ')
+				pp.pprint('Warning: Price not returned... ')
 				# entry['Price'] = 'NULL'
 				entry = dict()
 				entry['GTA_key'] = hotel_code['city_code'] + '_' + hotel_code['item_code']
 				res.append(entry)
-
 			else:
 				# pp.pprint('Gross: ' + str(r_tree.find('.//RoomPrice').get('Gross')))
+				r_tree = ET.fromstring(r.text)
+
 				for hotel in r_tree.find('.//HotelDetails'):
 					hotel_name = hotel.find('.//Item').text
 					for room_cat in r_tree.find('.//RoomCategories'):
@@ -142,13 +173,18 @@ def asp(file_name, from_d, to_d, client):
 			max_len = len(ent.keys())
 			keys = ent.keys()
 
+	output_file_name = '_'.join([ 'Output_search_price',
+									file_name,
+									from_date.strftime('%y%m%d'),
+									datetime.datetime.now().strftime('%H%M')
+								]) + '.csv'
+
 	# keys = res[0].keys()
 	# with open('output_SearchPrice_' + date.today().strftime('%Y_%m_%d') + '.csv', 'w', encoding='utf-8') as output_file:
-	with open('output_Search_price_' + file_name + '_' + from_date.strftime('%y_%m_%d') + '_' + datetime.datetime.now().strftime('%H%M') + '.csv', 'w', encoding='utf-8') as output_file:
+	with open(output_file_name, 'w', newline='', encoding='utf-8') as output_file:
 		dict_writer = csv.DictWriter(output_file, keys)
 		dict_writer.writeheader()
 		dict_writer.writerows(res)
-	
 
 if __name__ == '__main__':
 	asp()
