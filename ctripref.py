2f9985407cf520f30b9ede09d6898313bc82c332
Yu Leng <tctctcly@gmail.com>
2017-11-06 14:36:23 +0800

    add sendmail to ctripref

--- ctripref.py
+++ ctripref.py
@@ -110,5 +110,9 @@ def ctripref(days, duration):
 		return
 	subprocess.call(['python', 'ctrip_store_booking.py', '--filename', newest, '--days', '-30', '--output', 'output_ctrip_booking_store.csv'])
 
+	subprocess.call(['pyhton', 'sendmail_win_cs.py', 
+						'--filename', 'output_ctrip_update_res_no_', 
+						'--email', 'no-reply@gta-travel.com'])
+
 if __name__ == '__main__':
 	ctripref()
