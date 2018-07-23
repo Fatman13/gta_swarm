57bf5dbdb36c4fc6461545c1fdb83642ca5882cb
Leng <809452@apac.kuoni.int>
2018-07-23 16:32:50 +0800

    clean up code

--- hb_report.py
+++ hb_report.py
@@ -2,17 +2,8 @@
 # coding=utf-8
 
 import pprint
-# import csv
 import click 
-# import requests
-# import datetime as datetime
-# from datetime import date
-# from xml.etree import ElementTree as ET
 import os
-# from random import sample
-# import random
-# import json
-# import logging
 import subprocess
 import glob
 import time
@@ -56,73 +47,20 @@ def hua_style_sleep():
 		time.sleep(1)
 
 @click.command()
-# @click.option('--days', default=0, type=int)
-# @click.option('--duration', default=3, type=int)
-# @click.option('--days', default=1, type=int)
-# def hb_report(days, duration):
 def hb_report():
 
-	# subprocess.call(['python', 'booking_id_ctrip.py', '--days', str(days), '--duration', str(duration), '--d_type', 'departure'])
 	subprocess.call(['python', 'sendmail_win_hb.py', '--days', '-1'])
 
 	hua_style_sleep()
 
 	newest = max(glob.iglob('CTRIP---API-Errors---API-valuation-step-issues*.csv'), key=os.path.getctime)
 	print('Using.. ' + newest)
-	# if is_bad_date('output_Search_booking_id_(\d+)', newest):
-	# if is_bad_date_re('output_Search_booking_id_(\d+)', newest):
-	# 	print('Error: bad date.. ')
-	# 	return
+
 	subprocess.call(['python', 'hb_er.py', '--filename', newest])
 	# subprocess.call(['python', 'search_item_hr.py', '--filename', newest])
 
 	hua_style_sleep()
 	subprocess.call(['python', 'sendmail_win_hb2.py'])
-
-	# newest = max(glob.iglob('output_Search_item_hr_*.csv'), key=os.path.getctime)
-	# # if is_bad_date('output_Search_item_hr_(\d+)', newest):
-	# if is_bad_date_re('output_Search_item_hr_(\d+)', newest):
-	# 	print('Error: bad date.. ')
-	# 	return
-	# # subprocess.call(['python', 'hc.py', '--filename', newest])
-	# subprocess.call(['python', 'hc_pool.py', '--filename', newest])
-
-	# hua_style_sleep()
-
-	# # newest = max(glob.iglob('output_Search_item_hr_*.csv'), key=os.path.getctime)
-	# # subprocess.call(['python', 'sendmail.py', '--filename', 'output_hotel_ref_*.csv', '--title', 'Ctrip_hotel_ref'])
-
-	# newest = max(glob.iglob('output_hotel_ref_*.csv'), key=os.path.getctime)
-
-	# # if is_bad_date('output_hotel_ref_(\d+)', newest):
-	# if is_bad_date_re('output_hotel_ref_(\d+)', newest):
-	# 	print('Error: bad date.. ')
-	# 	return
-
-	# # while True:
-	# # 	sys.stdout.write("Would you like to proceed to call Ctrip's update hotel res no API? " + newest + " [Y/N]")
-	# # 	choice = input().lower()
-	# # 	if choice == 'y' or choice == 'yes':
-	# # 		break
-	# # 	if choice == 'n' or choice == 'no':
-	# # 		return
-
-	# subprocess.call(['python', 'ctrip_update_res_no.py', '--filename', newest])
-
-	# # keep track of bookings that has been pushed
-	# newest = max(glob.iglob('output_ctrip_update_res_no_*.csv'), key=os.path.getctime)
-	# # if is_bad_date('output_ctrip_update_res_no_(\d+)', newest):
-	# if is_bad_date_re('output_ctrip_update_res_no_(\d+)', newest):
-	# 	print('Error: bad date.. ')
-	# 	return
-	# subprocess.call(['python', 'ctrip_store_booking.py', '--filename', newest, '--days', '-30', '--output', 'output_ctrip_booking_store.csv'])
-
-	# # subprocess.call(['python', 'sendmail_win_cs.py', 
-	# # 					'--filename', 'output_hotel_ref_', 
-	# # 					'--email', 'no-reply@gta-travel.com'])
-	# subprocess.call(['python', 'sendmail_win_cs.py', 
-	# 					'--filename', 'output_ctrip_update_res_no_', 
-	# 					'--email', 'no-reply@gta-travel.com'])
 
 if __name__ == '__main__':
 	hb_report()
